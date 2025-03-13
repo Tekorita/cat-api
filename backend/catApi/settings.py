9fdffc507ceeded9ff2b0387b87db6c49d23a2a0
Tekorita <davidpalmalugo@gmail.com>
2025-03-13 14:11:03 -0400

    config postgres and make first migrations

--- backend/catApi/settings.py
+++ backend/catApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 import environ
+import dj_database_url 
 
 from pathlib import Path
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "catApi",
 ]
 
 MIDDLEWARE = [
@@ -78,10 +80,9 @@ WSGI_APPLICATION = "catApi.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://postgres:postgres@db:5432/catapi_db"
+    )
 }
 
 
