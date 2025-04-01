57542209873523bc6413d8ee985ad51dd04d635d
Tekorita <davidpalmalugo@gmail.com>
2025-03-31 23:58:43 -0400

    test with url production in frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = "http://cat-api-env-prod.eba-jbfd8qbt.us-east-1.elasticbeanstalk.com";
 
 export const fetchCats = async (playerName: string, difficulty: string) => {
   try {
